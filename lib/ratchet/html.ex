2ec6100cdb8964a88210f0eb5002e494c0a25bff
Jay Hayes <ur@iamvery.com>
2016-05-13 15:05:32 -0500

    Content is applied to an element

--- lib/ratchet/html.ex
+++ lib/ratchet/html.ex
@@ -1,2 +1,11 @@
 defmodule Ratchet.Html do
+  @doc """
+  Apply content to an element
+
+      iex> Html.apply({"div", [], []}, "Content")
+      {"div", [], ["Content"]}
+  """
+  def apply({tag, attributes, _children}, content) do
+    {tag, attributes, [content]}
+  end
 end
--- test/ratchet/html_test.exs
+++ test/ratchet/html_test.exs
@@ -2,4 +2,11 @@ defmodule Ratchet.HtmlTest do
   use ExUnit.Case, async: true
   alias Ratchet.Html
   doctest Html
+
+  test "apply content" do
+    element = {"div", [], []}
+    result = Html.apply(element, "Content")
+
+    assert result == {"div", [], ["Content"]}
+  end
 end
