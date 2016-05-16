1dd6275dfa9bd3299163d20e7844cd0008bbe8f4
Jay Hayes <ur@iamvery.com>
2016-05-16 09:53:39 -0500

    Add top level interface for transforming view as string

--- lib/ratchet.ex
+++ lib/ratchet.ex
@@ -1,2 +1,7 @@
 defmodule Ratchet do
+  def transform(view, scope, data) do
+    tree = Floki.parse(view)
+    result = Ratchet.Html.transform(tree, scope, data)
+    Floki.raw_html(result)
+  end
 end
