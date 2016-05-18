6eaf87385d777919e8558c0997c5e525c74fb066
Jay Hayes <ur@iamvery.com>
2016-05-18 14:17:39 -0500

    Implement transformer to insert EEx statements into AST

---
+++ lib/ratchet/transformer.ex
@@ -0,0 +1,63 @@
+defmodule Ratchet.Transformer do
+  import Ratchet.EEx
+
+  @doc """
+  Build EEx statements from Ratchet AST
+  """
+  def transform(_, scope \\ "data")
+
+  def transform([], _), do: []
+  def transform([child|rest], scope) do
+    [transform(child, scope)|transform(rest, scope)]
+  end
+
+  def transform(element, scope) do
+    case get_type(element) do
+      :none -> transform_element(element, scope)
+      type -> transform_element(type, element, scope)
+    end
+  end
+
+  @doc """
+  Get the "type" of a node. First property or scope is returned.
+
+      iex> Transformer.get_type({"div", [{"data-scope", "foo"}], []})
+      {:scope, "foo"}
+      iex> Transformer.get_type({"div", [{"data-prop", "bar"}], []})
+      {:property, "bar"}
+      iex> Transformer.get_type({"div", [{"data-scope", "foo"}, {"data-prop", "bar"}], []})
+      {:scope, "foo"}
+      iex> Transformer.get_type({"div", [], []})
+      :none
+  """
+  def get_type({_tag, attributes, _children}) do
+    Enum.find_value attributes, :none, fn
+      {"data-scope", scope} -> {:scope, scope}
+      {"data-prop", prop} -> {:property, prop}
+      _ -> false
+    end
+  end
+
+  defp transform_element({:scope, property}, {tag, attributes, children}, scope) do
+    children = transform(children, property) |> List.flatten
+    [
+      eex_comprehension_open(scope, property),
+      {tag, attributes, children},
+      eex_close,
+    ]
+  end
+
+  defp transform_element({:property, property}, {tag, attributes, _children}, scope) do
+    children = eex(property) |> List.wrap
+    [
+      eex_comprehension_open(scope, property),
+      {tag, attributes, children},
+      eex_close,
+    ]
+  end
+
+  defp transform_element({tag, attributes, children}, scope) do
+    children = transform(children, scope) |> List.flatten
+    {tag, attributes, children}
+  end
+end
---
+++ test/ratchet/transformer_test.exs
@@ -0,0 +1,27 @@
+defmodule Ratchet.TransformerTest do
+  use ExUnit.Case, async: true
+  alias Ratchet.Transformer
+  doctest Transformer
+
+  @ratchet_list_ast {"div", [], [
+      {"ul", [{"data-scope", "list"}], [
+        {"li", [{"data-prop", "name"}], []},
+      ]},
+    ]}
+
+  @eex_list_ast {"div", [], [
+        "<%= for list <- List.wrap(data.list) do %>",
+        {"ul", [{"data-scope", "list"}], [
+          "<%= for name <- List.wrap(list.name) do %>",
+          {"li", [{"data-prop", "name"}], ["<%= name %>"]},
+          "<% end %>",
+        ]},
+        "<% end %>",
+      ]}
+
+  test "transform/1 transforms tree to include EEx statements for scopes and properties" do
+    result = Transformer.transform(@ratchet_list_ast)
+
+    assert result == @eex_list_ast
+  end
+end
