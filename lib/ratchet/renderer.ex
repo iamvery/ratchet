ba8743a50972a3f87b43a4928ba7a4d8772fe1af
Jay Hayes <ur@iamvery.com>
2016-05-19 08:10:38 -0500

    Add examples to renderer docs

--- lib/ratchet/renderer.ex
+++ lib/ratchet/renderer.ex
@@ -3,6 +3,9 @@ defmodule Ratchet.Renderer do
 
   @doc """
   Render template to markup given data
+
+      iex> Renderer.render(~S{<div><p data-prop="txt"></p></div>}, %{txt: "Hi!"})
+      ~S{<div><p data-prop="txt">Hi!</p></div>}
   """
   def render(template, data) do
     template
@@ -14,6 +17,9 @@ defmodule Ratchet.Renderer do
 
   @doc """
   Parse template to AST
+
+      iex> Renderer.parse("<div></div>")
+      {"div", [], []}
   """
   def parse(template) do
     Floki.parse(template)
@@ -21,6 +27,9 @@ defmodule Ratchet.Renderer do
 
   @doc """
   Compile markup from AST
+
+      iex> Renderer.compile({"div", [], []})
+      "<div></div>"
   """
   def compile(ast) do
     Floki.raw_html(ast)
