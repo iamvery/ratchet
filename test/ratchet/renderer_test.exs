f88938279b2fe63d04f4418b35cd4371c7ee814b
Jay Hayes <ur@iamvery.com>
2016-05-23 16:50:05 -0500

    Update high-level renderer test to include attributes

--- test/ratchet/renderer_test.exs
+++ test/ratchet/renderer_test.exs
@@ -7,6 +7,7 @@ defmodule Ratchet.RendererTest do
   <section>
     <article data-scope="posts">
       <p data-prop="body"></p>
+      <a data-prop="link"></a>
       <ul>
         <li data-prop="comments"></li>
       </ul>
@@ -16,20 +17,22 @@ defmodule Ratchet.RendererTest do
 
   test "render/2" do
     data = %{posts: [
-        %{body: "Thoughts and opinions.", comments: ["I disagree."]},
-        %{body: "JavaScript is dead.", comments: ["WAT", "YAY"]},
+        %{body: "Thoughts and opinions.", link: {"Google", href: "https://google.com"}, comments: ["I disagree."]},
+        %{body: "JavaScript is dead.", link: {"Iamvery", href: "https://iamvery.com"}, comments: ["WAT", "YAY"]},
       ]}
 
     rendered = """
     <section>
       <article data-scope="posts">
         <p data-prop="body">Thoughts and opinions.</p>
+        <a href="https://google.com" data-prop="link">Google</a>
         <ul>
           <li data-prop="comments">I disagree.</li>
         </ul>
       </article>
       <article data-scope="posts">
         <p data-prop="body">JavaScript is dead.</p>
+        <a href="https://iamvery.com" data-prop="link">Iamvery</a>
         <ul>
           <li data-prop="comments">WAT</li>
           <li data-prop="comments">YAY</li>
