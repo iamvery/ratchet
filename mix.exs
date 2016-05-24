d8590c9ea6d65fc1167a9c02e249f1e91b19cb54
Jay Hayes <ur@iamvery.com>
2016-05-24 16:52:17 -0500

    v0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ratchet.Mixfile do
 
   def project do
     [app: :ratchet,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
