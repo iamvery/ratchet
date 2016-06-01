0d45d6da0ee4521dee18c37097f901dfe9e15b8a
Jay Hayes <ur@iamvery.com>
2016-06-01 18:56:23 -0500

    Extract module attribute for project version

--- mix.exs
+++ mix.exs
@@ -1,9 +1,11 @@
 defmodule Ratchet.Mixfile do
   use Mix.Project
 
+  @version "0.3.0"
+
   def project do
     [app: :ratchet,
-     version: "0.3.0",
+     version: @version,
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
