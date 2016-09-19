af15b947d022b4cd35544a888af8e5539b86b9b0
Jay Hayes <ur@iamvery.com>
2016-09-19 11:44:06 -0500

    Update implementation to support struct data
    Elixir structs do not implement the Access protocol which is used by
    []/1. However, structs are compatible with Map.* as they are so called
    "bare" maps.

--- test/ratchet/template_test.exs
+++ test/ratchet/template_test.exs
@@ -3,6 +3,16 @@ defmodule Ratchet.TemplateTest do
   alias Ratchet.Template
   doctest Template
 
+  describe "property/2" do
+    defmodule Foo do
+      defstruct bar: nil
+    end
+
+    test "properties may be fetched from structs" do
+      assert Template.property(%Foo{bar: "baz"}, :bar) == "baz"
+    end
+  end
+
   test "attributes names are safe" do
     attributes = Template.attributes(nil, [{~S{onclick="alert('HACKED!')" class}, ""}])
 
